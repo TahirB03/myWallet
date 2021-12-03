2b217ea51b712bd5be92cde762551fcf04893112
Gazmir Gaz <gazmir1000@gmail.com>
2021-12-03 19:30:57 +0100

    init

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const connectDB = require("./config/dbConnection");
+connectDB();
+
+const PORT = process.env.PORT || 3000;
+mongoose.connection.once("open", () => {
+  console.log("Connected to mongoDB");
+  app.listen(PORT, () => console.log(`Server Started on port ${PORT}`));
+});
