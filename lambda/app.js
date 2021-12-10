2e81d9b3efa13fdf63a82b40c4006e1ead07d8d6
Tahir Baku <eri.baku3@gmail.com>
2021-12-10 12:27:20 +0100

    Category and user serverless

--- lambda/app.js
+++ lambda/app.js
@@ -3,8 +3,9 @@ const Category = require("./category");
 const User = require("./user");
 const connection = require("./db");
 const ObjectId = require("mongodb").ObjectId;
-const validator = require('email-validator')
-const {phone} = require('phone')
+const validator = require("email-validator");
+const { phone } = require("phone");
+const user = require("./user");
 
 //Get all the users (This is only for dev)
 const getAllUsers = async () => {
@@ -103,20 +104,27 @@ const createUser = async (event) => {
   if (event.body !== null && event.body !== undefined) {
     body = JSON.parse(event.body);
   }
-  if (!/^[A-Za-z]+$/.test(body.name) || !/^[A-Za-z]+$/.test(body.surname)){
+  if (!/^[A-Za-z]+$/.test(body.name) || !/^[A-Za-z]+$/.test(body.surname)) {
     return {
-        statusCode: 400,
-        headers: {
-          "Access-Control-Allow-Headers": "Content-Type",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
-        },
-        body: JSON.stringify({
-          message: "Name and Surname should not contain number and symbols",
-        }),
-      };
+      statusCode: 400,
+      headers: {
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+      },
+      body: JSON.stringify({
+        message: "Name and Surname should not contain number and symbols",
+      }),
+    };
   }
-  if (!("name" in body) && !("surname" in body) && !("age" in body) && !("email" in body) && !("phoneNumber" in body) && !("balance" in body)){
+  if (
+    !("name" in body) &&
+    !("surname" in body) &&
+    !("age" in body) &&
+    !("email" in body) &&
+    !("phoneNumber" in body) &&
+    !("balance" in body)
+  ) {
     return {
       statusCode: 400,
       headers: {
@@ -129,47 +137,124 @@ const createUser = async (event) => {
       }),
     };
   }
-  if (body.name.length < 3 || body.name.length > 20){
+  if (body.name.length < 3 || body.name.length > 20) {
     return {
-        statusCode: 400,
-        headers: {
-          "Access-Control-Allow-Headers": "Content-Type",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
-        },
-        body: JSON.stringify({
-          message: "Name should be longer than 3 characters and shorter than 20 characters",
-        }),
-      };
+      statusCode: 400,
+      headers: {
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+      },
+      body: JSON.stringify({
+        message:
+          "Name should be longer than 3 characters and shorter than 20 characters",
+      }),
+    };
   }
-  if (body.surname.length < 3 || body.surname.length > 20){
+  if (body.surname.length < 3 || body.surname.length > 20) {
     return {
-        statusCode: 400,
-        headers: {
-          "Access-Control-Allow-Headers": "Content-Type",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
-        },
-        body: JSON.stringify({
-          message: "Surname should be longer than 3 characters and shorter than 20 characters",
-        }),
-      };
+      statusCode: 400,
+      headers: {
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+      },
+      body: JSON.stringify({
+        message:
+          "Surname should be longer than 3 characters and shorter than 20 characters",
+      }),
+    };
   }
-  if (body.age < 16){
+  if (body.age < 16) {
     return {
-        statusCode: 400,
-        headers: {
-          "Access-Control-Allow-Headers": "Content-Type",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
-        },
-        body: JSON.stringify({
-          message: "You need to be older than 16 years old to use this app",
-        }),
-      };
+      statusCode: 400,
+      headers: {
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+      },
+      body: JSON.stringify({
+        message: "You need to be older than 16 years old to use this app",
+      }),
+    };
   }
-  if (!validator.validate(body.email)){
+  if (!validator.validate(body.email)) {
     return {
+      statusCode: 400,
+      headers: {
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+      },
+      body: JSON.stringify({
+        message: "Check your email!",
+      }),
+    };
+  }
+  if (!phone(body.phoneNumber).isValid) {
+    return {
+      statusCode: 400,
+      headers: {
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+      },
+      body: JSON.stringify({
+        message: "Check your phone number!",
+      }),
+    };
+  }
+  if (body.balance < 0) {
+    return {
+      statusCode: 400,
+      headers: {
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+      },
+      body: JSON.stringify({
+        message: "Balance needs to be greater than 0",
+      }),
+    };
+  }
+  try {
+    const user = await User.create(body);
+    return {
+      statusCode: 201,
+      headers: {
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+      },
+      body: JSON.stringify({
+        user: user,
+      }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 400,
+      headers: {
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+      },
+      body: JSON.stringify({
+        message: "Something went wrong",
+        reason: error,
+      }),
+    };
+  }
+};
+//Update a user
+const updateUser = async (event) => {
+  const userId = event.pathParameters.id;
+  let body;
+  if (event.body !== null && event.body !== undefined) {
+    body = JSON.parse(event.body);
+  }
+  if (body.email !== undefined) {
+    if (!(validator.validate(body.email))) {
+      return {
         statusCode: 400,
         headers: {
           "Access-Control-Allow-Headers": "Content-Type",
@@ -180,9 +265,11 @@ const createUser = async (event) => {
           message: "Check your email!",
         }),
       };
+    }
   }
-  if (!phone(body.phoneNumber).isValid){
-    return {
+  if (body.phoneNumber!==undefined) {
+    if (!(phone(body.phoneNumber).isValid)) {
+      return {
         statusCode: 400,
         headers: {
           "Access-Control-Allow-Headers": "Content-Type",
@@ -193,50 +280,215 @@ const createUser = async (event) => {
           message: "Check your phone number!",
         }),
       };
+    }
   }
-  if (body.balance < 0){
+  if (!ObjectId.isValid(userId)) {
     return {
-        statusCode: 400,
-        headers: {
-          "Access-Control-Allow-Headers": "Content-Type",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
-        },
-        body: JSON.stringify({
-          message: "Balance needs to be greater than 0",
-        }),
-      };
+      statusCode: 400,
+      headers: {
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+      },
+      body: JSON.stringify({
+        message: "Make sure the ID is correct",
+      }),
+    };
   }
   try {
-     const user = await User.create(body);
-     return {
-        statusCode: 201,
-        headers: {
-          "Access-Control-Allow-Headers": "Content-Type",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
-        },
-        body: JSON.stringify({
-          user: user,
-        }),
-      };
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        email: body.email,
+        phoneNumber: body.phoneNumber,
+      },
+      { new: true }
+    );
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+      },
+      body: JSON.stringify({
+        message: "User updated succsefully",
+        user: updatedUser,
+      }),
+    };
   } catch (error) {
     return {
-        statusCode: 400,
-        headers: {
-          "Access-Control-Allow-Headers": "Content-Type",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
-        },
-        body: JSON.stringify({
-          message: "Something went wrong",
-          reason: error
-        }),
-      };
+      statusCode: 400,
+      headers: {
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+      },
+      body: JSON.stringify({
+        message: "Something went wrong",
+        reason: error,
+      }),
+    };
   }
 };
+//Delete a user
+const deleteUser = async (event)=>{
+  const userId = event.pathParameters.id;
+  if (!ObjectId.isValid(userId)) {
+    return {
+      statusCode: 400,
+      headers: {
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+      },
+      body: JSON.stringify({
+        message: "Make sure the ID is correct",
+      }),
+    };
+  }
+  try {
+     await User.findByIdAndDelete(userId);
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+      },
+      body: JSON.stringify({
+        message: "User succesfully deleted",
+      }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 400,
+      headers: {
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+      },
+      body: JSON.stringify({
+        message: "Something went wrong",
+        reason: error,
+      }),
+    };
+  }
+}
+////////////////////////////////////////////////////////////////////// CATEGORY SECTION  ///////////////////////////////////////////////////
+// Get all categorie
+const getAllCategories = async ()=>{
+  try {
+    const allCategories = await Category.find();
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+      },
+      body: JSON.stringify({
+        allCategories: allCategories,
+      }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 400,
+      headers: {
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+      },
+      body: JSON.stringify({
+        message: "Something went wrong",
+        reason: error
+      }),
+    };
+  }
+}
+//Get category from ID
+const getCategoryFromId = async event =>{
+  const categoryId = event.pathParameters.id;
+  if (!ObjectId.isValid(categoryId)) {
+    return {
+      statusCode: 400,
+      headers: {
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+      },
+      body: JSON.stringify({
+        message: "Make sure the ID is correct",
+      }),
+    };
+  }
+  try {
+    const category = await Category.findById(categoryId);
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+      },
+      body: JSON.stringify({
+        category: category,
+      }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 400,
+      headers: {
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+      },
+      body: JSON.stringify({
+        message: "something went wrong",
+        reason: error
+      }),
+    };
+  }
+}
+//Delete Category
+const deleteCategory = async event =>{
+  const categoryId = event.pathParameters.id;
+  try {
+        await Category.findByIdAndDelete(categoryId)
+        return {
+          statusCode: 200,
+          headers: {
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+          },
+          body: JSON.stringify({
+            message: "Category succesfully deleted",
+            
+          }),
+        };
+  } catch (error) {
+    return {
+      statusCode: 400,
+      headers: {
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+      },
+      body: JSON.stringify({
+        message: "Something went wrong",
+        reason: error,
+      }),
+    };
+  }
+}
 module.exports = {
   getAllUsers,
   getUserById,
   createUser,
+  updateUser,
+  deleteUser,
+  getAllCategories,
+  getCategoryFromId,
+  deleteCategory,
 };
