883ac6e6aab77d9f6dae4b3ec1262a4fd58ed60d
Gazmir Gaz <gazmir1000@gmail.com>
2021-12-13 18:14:44 +0100

    navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
-import React,{useState} from "react";
+import React, { useState } from "react";
 import poolData from "./poolData";
-import Amplify, { Auth } from 'aws-amplify';
+import Amplify, { Auth } from "aws-amplify";
 import { AmplifySignOut } from "@aws-amplify/ui-react";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
 // import { ConfirmSignIn, ConfirmSignUp, ForgotPassword, RequireNewPassword, SignUp, VerifyContact, withAuthenticator , } from 'aws-amplify-react';
 // import CostumFlow from './signIn/CostumFlow'
 Amplify.configure(poolData);
@@ -14,6 +16,7 @@ function App() {
   return (
     <div className="App">
       <div>
+        <Navbar />
         <Router>
           <Routes>
             <Route exact path="/" element={<Home />}></Route>
