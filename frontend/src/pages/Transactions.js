0013c4bc8a4ce9d751025a11e3e60cb0f32c0ae2
Gazmir Gaz <gazmir1000@gmail.com>
2021-12-15 00:06:49 +0100

    render values

--- frontend/src/pages/Transactions.js
+++ frontend/src/pages/Transactions.js
@@ -1,12 +1,27 @@
-import { Typography } from "@mui/material";
-import React from "react";
+import { TextField, Typography } from "@mui/material";
+import CompareArrowsIcon from "@mui/icons-material/CompareArrows";
+import React, { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import "./transactions.css";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import axios from "axios";
 
 export const Transactions = () => {
+  const url =
+    "https://nx1qh9klx1.execute-api.eu-south-1.amazonaws.com/dev/events/getEventByDate/fc099009-220e-44eb-9900-ead7e2e0613b";
+  const [eventValues, setEventValues] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    axios.post(url, { startingDate: 2020 - 12 - 10 }).then((response) => {
+      console.log(response);
+      console.log(response.data.events);
+      setEventValues(response.data.events);
+      setLoading(true);
+    });
+  }, [url]);
   const settings = {
     dots: false,
     infinite: true,
@@ -31,9 +46,22 @@ export const Transactions = () => {
         <button className="months">Nov</button>
         <button className="months">Dec</button>
       </Slider>
+
       <div className="stats">
         <Typography className="typo">Income</Typography>
         <Typography className="typo">Outcome</Typography>
+      </div>
+      <div className="arrows">
+        <CompareArrowsIcon />
+        Transactions
+      </div>
+      <div className="amount">
+        {loading &&
+          eventValues.map((value) => (
+            <div className="amountChild" key={value._id}>
+              {value.amount}$
+            </div>
+          ))}
       </div>
     </div>
   );
